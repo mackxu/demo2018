5e1eedfd6b78aea354632803274e5a3e988be503
xuyadong <xuyadong@qiyi.com>
2018-03-24 20:41:39 +0800

    supper less

--- testvue/src/App.vue
+++ testvue/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <a @click="doClick">Toast</a>
     <HelloWorld/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import toast from './components/toast/function-toast'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    doClick () {
+      toast({
+        content: 'Toast! Toast!'
+      })
+    }
   }
 }
 </script>
