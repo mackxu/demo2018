6262dcf3820eff6e910d11c3fec3d007d4efb8d1
mackxu <xydjob@gmail.com>
2018-04-15 21:57:49 +0800

    cookie 方法待实现

---
+++ testvue/src/tools/cookie.js
@@ -0,0 +1,8 @@
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Document/cookie
+export default class DocCookie {
+  getItem(name) {}
+  setItem(name, value) {}
+  removeItem(name) {}
+  hasItem() {name}
+  keys() {}
+}
