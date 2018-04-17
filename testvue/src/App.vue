2fe2ac842d23ede373ef5147c5b4ac4faccdc283
xuyadong <xuyadong@qiyi.com>
2018-04-17 21:25:14 +0800

    modify

--- testvue/src/App.vue
+++ testvue/src/App.vue
@@ -13,8 +13,6 @@ export default {
   name: 'App',
   methods: {
     doClick () {
-      import('./assets/logo.png')
-      import('./assets/Penguins.jpg')
       toast({
         content: 'Toast! Toast!'
       })
@@ -27,12 +25,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
