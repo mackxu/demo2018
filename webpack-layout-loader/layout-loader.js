a2930dddb31998acd707ada280785b1be5a964d9
xuyadong <xuyadong@qiyi.com>
2018-10-26 10:08:32 +0800

    fixed 同步loader使用return

--- webpack-layout-loader/layout-loader.js
+++ webpack-layout-loader/layout-loader.js
@@ -1,17 +1,16 @@
 // 执行在node环境，返回一个函数
 const fs = require('fs')
-const path = require('path')
 const loaderUtils = require('loader-utils')
 
 module.exports = function layoutLoader(source) {
-  const callback = this.async();
   const options = loaderUtils.getOptions(this);
   const layoutPath = options.layout;
+  console.log(`layoutPath: ${layoutPath}`);
   try {
     const layoutHtml = fs.readFileSync(layoutPath, 'utf-8')
     source = layoutHtml.replace('{{__content__}}', source)
-    callback(null, `export default ${ JSON.stringify(source) }`);
+    return `export default ${ JSON.stringify(source) }`;
   } catch (error) {
-    callback(error)
+    return error;
   }
 }
